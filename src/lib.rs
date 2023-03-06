053abe7f3ec1742e7d07a22f053afd45300fa57c
Ji Chen <jaykchen@gmail.com>
2023-03-05 21:52:37 -0500

    optimized how varaibles are assigned

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,26 @@ use openai_flows::chat_completion;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    listen_to_event(
-        "second-state",
-        "chat-with-chatgpt",
-        vec!["issue_comment", "issues"],
-        handler,
-    )
+    let owner = "second-state";
+    let repo = "chat-with-chatgpt";
+
+    listen_to_event(owner, repo, vec!["issue_comment", "issues"], |payload| {
+        handler(owner, repo, payload)
+    })
     .await;
 }
 
-async fn handler(payload: EventPayload) {
-    let octo = get_octo(Some(String::from("second-state")));
-    let issues = octo.issues("second-state", "chat-with-chatgpt");
+async fn handler(owner: &str, repo: &str, payload: EventPayload) {
+    let octo = get_octo(Some(String::from(owner)));
+    let issues = octo.issues(owner, repo);
 
+    let openpi_key_name = "chatmichael";
     match payload {
         EventPayload::IssueCommentEvent(e) => {
             if e.comment.user.r#type != "Bot" {
                 if let Some(b) = e.comment.body {
                     if let Some(r) =
-                        chat_completion("chatmichael", &format!("issue#{}", e.issue.number), &b)
+                        chat_completion(openpi_key_name, &format!("issue#{}", e.issue.number), &b)
                     {
                         if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
                             write_error_log!(e.to_string());
@@ -37,7 +38,9 @@ async fn handler(payload: EventPayload) {
             let title = e.issue.title;
             let body = e.issue.body.unwrap_or("".to_string());
             let q = title + "\n" + &body;
-            if let Some(r) = chat_completion("chatmichael", &format!("issue#{}", e.issue.number), &q) {
+            if let Some(r) =
+                chat_completion(openpi_key_name, &format!("issue#{}", e.issue.number), &q)
+            {
                 if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
                     write_error_log!(e.to_string());
                 }
