6e06575142af36ad712ddf8f6d0f0bf6d328f79e
Ji Chen <jaykchen@gmail.com>
2023-03-07 09:54:11 -0500

    solved circular referal of issue structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 use flowsnet_platform_sdk::write_error_log;
-use github_flows::{get_octo, listen_to_event, octocrab::models::events::payload::EventPayload};
+use github_flows::{
+    get_octo, listen_to_event,
+    octocrab::{
+        actions, models::events::payload::EventPayload, models::events::payload::IssuesEventAction,
+    },
+};
 use openai_flows::chat_completion;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    let owner = "second-state";
-    let repo = "chat-with-chatgpt";
+    let owner = "jaykchen";
+    let repo = "vitesse-lite";
+    // let owner = "second-state";
+    // let repo = "chat-with-chatgpt";
 
-    listen_to_event(owner, repo, vec!["issue_comment", "issues"], |payload| {
+    listen_to_event(owner, repo, vec!["issue_comment"], |payload| {
         handler(owner, repo, payload)
     })
     .await;
@@ -18,7 +25,9 @@ async fn handler(owner: &str, repo: &str, payload: EventPayload) {
     let octo = get_octo(Some(String::from(owner)));
     let issues = octo.issues(owner, repo);
 
-    let openpi_key_name = "chatmichael";
+    let openpi_key_name = "jaykchen";
+    // let openpi_key_name = "chatmichael";
+
     match payload {
         EventPayload::IssueCommentEvent(e) => {
             if e.comment.user.r#type != "Bot" {
@@ -35,6 +44,10 @@ async fn handler(owner: &str, repo: &str, payload: EventPayload) {
         }
 
         EventPayload::IssuesEvent(e) => {
+            if e.action == IssuesEventAction::Closed {
+                return;
+            }
+
             let title = e.issue.title;
             let body = e.issue.body.unwrap_or("".to_string());
             let q = title + "\n" + &body;
