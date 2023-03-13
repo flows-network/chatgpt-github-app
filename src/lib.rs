2c7621f0bdc20d15f3811b9d6418d1a87481bdf1
DarumaDocker <DarumaDocker@gmail.com>
2023-03-13 18:58:36 +0800

    update to openai sdk 0.2

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use github_flows::{
     get_octo, listen_to_event,
     octocrab::{models::events::payload::EventPayload, models::events::payload::IssuesEventAction},
 };
-use openai_flows::chat_completion;
+use openai_flows::{chat_completion, ChatOptions};
 use std::env;
 
 #[no_mangle]
@@ -41,9 +41,12 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
         EventPayload::IssueCommentEvent(e) => {
             if e.comment.user.r#type != "Bot" {
                 if let Some(b) = e.comment.body {
-                    if let Some(r) =
-                        chat_completion(openai_key_name, &format!("issue#{}", e.issue.number), &b)
-                    {
+                    if let Some(r) = chat_completion(
+                        openai_key_name,
+                        &format!("issue#{}", e.issue.number),
+                        &b,
+                        &ChatOptions::default(),
+                    ) {
                         if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
                             write_error_log!(e.to_string());
                         }
@@ -60,9 +63,12 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
             let title = e.issue.title;
             let body = e.issue.body.unwrap_or("".to_string());
             let q = title + "\n" + &body;
-            if let Some(r) =
-                chat_completion(openai_key_name, &format!("issue#{}", e.issue.number), &q)
-            {
+            if let Some(r) = chat_completion(
+                openai_key_name,
+                &format!("issue#{}", e.issue.number),
+                &q,
+                &ChatOptions::default(),
+            ) {
                 if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
                     write_error_log!(e.to_string());
                 }
