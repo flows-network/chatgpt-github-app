923c0725dc7ec70dce2bbcd3fdff50d0295b1108
DarumaDocker <DarumaDocker@gmail.com>
2023-03-24 14:47:45 +0800

    update to github integration 0.4

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,18 @@ use github_flows::{
     get_octo, listen_to_event,
     octocrab::{models::events::payload::EventPayload, models::events::payload::IssuesEventAction},
 };
-use openai_flows::{chat_completion, ChatOptions, ChatModel};
+use openai_flows::{chat_completion, ChatModel, ChatOptions};
 use std::env;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     dotenv().ok();
+
+    let login: String = match env::var("login") {
+        Err(_) => "alabulei1".to_string(),
+        Ok(name) => name,
+    };
 
     let owner: String = match env::var("owner") {
         Err(_) => "second-state".to_string(),
@@ -27,9 +32,13 @@ pub async fn run() {
         Ok(name) => name,
     };
 
-    listen_to_event(&owner, &repo, vec!["issue_comment", "issues"], |payload| {
-        handler(&owner, &repo, &openai_key_name, payload)
-    })
+    listen_to_event(
+        &login,
+        &owner,
+        &repo,
+        vec!["issue_comment", "issues"],
+        |payload| handler(&owner, &repo, &openai_key_name, payload),
+    )
     .await;
 }
 
@@ -37,10 +46,10 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
     let octo = get_octo(Some(String::from(owner)));
     let issues = octo.issues(owner, repo);
 
-
     match payload {
         EventPayload::IssueCommentEvent(e) => {
-            if e.comment.user.r#type != "Bot" {
+            let last_comment_id = store_flows::get("last_created_comment").unwrap_or_default();
+            if e.comment.id.into_inner() != last_comment_id.as_u64().unwrap_or_default() {
                 if let Some(b) = e.comment.body {
                     if let Some(r) = chat_completion(
                         openai_key_name,
@@ -48,8 +57,16 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
                         &b,
                         &ChatOptions::default(),
                     ) {
-                        if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
-                            write_error_log!(e.to_string());
+                        match issues.create_comment(e.issue.number, r.choice).await {
+                            Ok(comment) => {
+                                store_flows::set(
+                                    "last_created_comment",
+                                    serde_json::to_value(comment.id.into_inner()).unwrap(),
+                                );
+                            }
+                            Err(e) => {
+                                write_error_log!(e.to_string());
+                            }
                         }
                     }
                 }
@@ -69,7 +86,7 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
             let co = ChatOptions {
                 model: ChatModel::GPT4,
                 restart: true,
-                restarted_sentence: Some(&prompt)
+                restarted_sentence: Some(&prompt),
             };
 
             if let Some(r) = chat_completion(
@@ -78,8 +95,16 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
                 &prompt,
                 &co,
             ) {
-                if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
-                    write_error_log!(e.to_string());
+                match issues.create_comment(e.issue.number, r.choice).await {
+                    Ok(comment) => {
+                        store_flows::set(
+                            "last_created_comment",
+                            serde_json::to_value(comment.id.into_inner()).unwrap(),
+                        );
+                    }
+                    Err(e) => {
+                        write_error_log!(e.to_string());
+                    }
                 }
             }
         }
